6f7670d4ac8e0f612dc6e1afb8ff29f479269ce1
fargelus <ddraudred@gmail.com>
2018-11-07 09:21:30 +0300

    tac.pl added

---
+++ tac.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+use strict;
+
+sub change_stderr {
+  unless (open STDERR, '>>files/error.log') {
+    die "Could not opened error log file\n";
+  }
+}
+
+sub get_file_content {
+  my $filename = $_[0];
+  unless (open "FILE", $filename) {
+    die "error while opening";
+  }
+
+  my @lines;
+  while (chomp(my $line = <FILE>)) {
+    push @lines, $line;
+  }
+  @lines;
+}
+
+change_stderr();
+
+my @files = @ARGV;
+foreach my $file (@files) {
+  my @content = get_file_content($file);
+  my @reversed = reverse @content;
+  print "@reversed\n";
+}
