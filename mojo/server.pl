23a5c39220ef012e8b317dd350ac6ac87b275b59
fargelus <ddraudred@gmail.com>
2018-12-29 21:07:45 +0300

    Printed username when submit

--- mojo/server.pl
+++ mojo/server.pl
@@ -1,13 +1,33 @@
 #!/usr/bin/env perl
-use strict;
 use Mojolicious::Lite;
 
-get '/' => 'index';
+get '/' => sub {
+  my $self = shift;
+  $self->render(text => 'Hello, world!!!');
+};
+
+get '/form' => sub {
+  my $self = shift;
+  $self->render('form');
+};
+
+post '/form' => sub {
+  my $self = shift;
+  my $username = $self->param('username');
+  $self->render('greetings', username => $username);
+};
+
 app->start;
 
 __DATA__
 
-@@ index.html.ep
-% layout 'default';
-% title 'Превееед!';
-<p> Test </p>
+@@ form.html.ep
+% title 'Form';
+<form method="post" action="/form">
+  <input type="text" name="username" placeholder="Введите имя" required pattern="[А-Яа-яЁё]+">
+  <button>Отправить</button>
+</form>
+
+@@ greetings.html.ep
+% title 'Greetings';
+<p>Привет, <%=$username%></p>
