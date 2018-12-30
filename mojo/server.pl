578fecf07d1909b371457fa19d87125188682fd5
fargelus <ddraudred@gmail.com>
2018-12-30 13:56:29 +0300

    Add db

--- mojo/server.pl
+++ mojo/server.pl
@@ -1,34 +1,54 @@
 use Mojolicious::Lite;
+use DBI;
+
+my $dbh = DBI->connect('dbi:SQLite:database.db', '', '') or die 'Could not connect';
 
 get '/' => sub {
   my $self = shift;
-  $self->render(text => 'Hello, world!!!');
-};
 
-get '/form' => sub {
-  my $self = shift;
-  my $is_logout = $self->param('logout');
-  my $username;
-
-  if ($is_logout) {
-    delete $self->session->{'username'};
-  } else {
-    $username = $self->session('username');
-  }
-
-  if (defined $username) {
+  my $username = $self->session('username');
+  if ($username) {
     $self->render('greetings', username => $username);
   } else {
-    $self->render('form');
+    $self->render('index');
   }
 };
 
-post '/greetings' => sub {
+get '/login' => sub {
+  my $self = shift;
+  $self->render('login');
+};
+
+post '/login' => sub {
+  my $self = shift;
+  my ($username, $pwd) = (
+    $self->param('username'),
+    $self->param('password')
+  );
+
+  # TODO Авторизация юзера
+  $self->render('greetings', username => $username);
+};
+
+get '/reg' => sub {
+  my $self = shift;
+  $self->render('registration');
+};
+
+post '/reg' => sub {
   my $self = shift;
   my $username = $self->param('username');
+  my $pwd = $self->param('password');
 
+  # TODO Регистрация юзера
   $self->session('username' => $username);
   $self->render('greetings', username => $username);
+};
+
+get '/logout' => sub {
+  my $self = shift;
+  delete $self->session->{'username'};
+  $self->render('index');
 };
 
 app->start;
