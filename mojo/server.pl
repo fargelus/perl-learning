499594c976da7dc3f0a64f5a5379601dc709a0cf
fargelus <ddraudred@gmail.com>
2018-12-30 18:41:23 +0300

    Reg and Auth

--- mojo/server.pl
+++ mojo/server.pl
@@ -3,6 +3,44 @@ use DBI;
 
 my $dbh = DBI->connect('dbi:SQLite:database.db', '', '') or die 'Could not connect';
 
+helper db => sub { $dbh };
+
+helper create_table => sub {
+  my $self = shift;
+  warn "Creating table 'users'\n";
+  $self->db->do('CREATE TABLE users (name varchar(255), pwd varchar(255))');
+};
+
+helper select_all => sub {
+  my $self = shift;
+  my $db = eval {$self->db->prepare("SELECT * FROM users")} || return undef;
+
+  $db->execute();
+  $db->fetchall_arrayref;
+};
+
+helper select => sub {
+  my $self = shift;
+  my ($looking_name, $looking_pwd) = @_;
+  my $query = "SELECT name, pwd FROM users WHERE name='$looking_name' AND pwd='$looking_pwd'";
+  my $db = eval {$self->db->prepare($query)} || return undef;
+
+  $db->execute();
+  $db->fetchrow_arrayref;
+};
+
+helper insert => sub {
+  my $self = shift;
+  my ($name, $pwd) = @_;
+  my $query = 'INSERT INTO users VALUES(?, ?)';
+  my $db = eval {$self->db->prepare($query)} || return undef;
+
+  $db->execute($name, $pwd);
+  1;
+};
+
+app->select_all() || app->create_table();
+
 get '/' => sub {
   my $self = shift;
 
@@ -26,8 +64,13 @@ post '/login' => sub {
     $self->param('password')
   );
 
-  # TODO Авторизация юзера
-  $self->render('greetings', username => $username);
+  my $target_rec = $self->select($username, $pwd);
+  if ($target_rec) {
+    $self->session('username' => $username);
+    $self->render('greetings', username => $username);
+  } else {
+    $self->render(text => "Пользователя $username не существует в базе");
+  }
 };
 
 get '/reg' => sub {
@@ -40,9 +83,13 @@ post '/reg' => sub {
   my $username = $self->param('username');
   my $pwd = $self->param('password');
 
-  # TODO Регистрация юзера
   $self->session('username' => $username);
-  $self->render('greetings', username => $username);
+  my $insert = $self->insert($username, $pwd);
+  if ($insert) {
+    $self->render('greetings', username => $username);
+  } else {
+    $self->render(text => "Не удалось зарегистрировать пользователя $username");
+  }
 };
 
 get '/logout' => sub {
