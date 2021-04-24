40eeac8d6f1e69627c851f6bab577014d673a508
Dmitriy Nemykin <ddraudred@gmail.com>
2021-04-24 11:52:20 +0300

    Simple CSV parser example

---
+++ regex/csv_parser.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+
+$line = 'Ten Thousand,10000, 2710,,"10000",10k,"It\'s "" 10 Grand"", baby"';
+while ($line =~ m/([^",]+)|"((?:[^"]|"")*)"/g) {
+  if (defined $1) {
+    $field = $1;
+  } else {
+    $field = $2;
+    $field =~ s/""/"/g;
+  }
+  print "[$field]";
+}
+print "\n";
